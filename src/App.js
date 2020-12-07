828329b88250ea75ab6578bdb42be0d0d71226ba
aditi <garg.aditi6@tcs.com>
2020-12-07 14:15:41 +0530

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router, Route , Redirect, Switch} from 'react-router-dom';
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App  =()  => {
+  return (<Router>
+    <Switch>
+    <Route path ="/" exact>
+      <Users />
+    </Route>
+    <Route path ="/place/new" exact>
+      <NewPlace />
+    </Route>
+    <Redirect to= '/' ></Redirect>
+    </Switch>
+    </Router>
+    );
+};
+export default App;
+ 
---
+++ src/users/pages/Users.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import UsersList from '../components/UsersList';
+import UsersList from '../components/UsersLists';
+
+const Users = () => {
+    const USERS =[{
+        id: 'u1',
+        name: 'Aditi',
+        image: 'https://media.gettyimages.com/photos/weve-made-it-all-this-way-i-am-proud-picture-id904172104?s=612x612',
+        places: 2
+    }];
+    return <UsersList items={USERS} />;
+};
+export default Users;
